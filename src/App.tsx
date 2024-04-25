488ed33f828c19e56cbc526f63f956f9008e1347
JuanDVejarano <juanjdv_200@hotmail.com>
2024-04-25 17:12:56 -0500

    Controles de tiempo y datos de tiempos de ejecucion

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,18 @@ function App() {
         status: string;
     }
 
+    // estados de las tareas
     const [tasks, setTasks] = useState<taskInterface[]>([]);
-
     const [tasksDone, setTasksDone] = useState<taskInterface[]>([]);
     const [tasksCanceled, setTasksCanceled] = useState<taskInterface[]>([]);
     let [taskID, setTaskID] = useState(0);
+    let [tiempo, setTiempo] = useState(100);
 
+    let [standby, setStandby] = useState(0); // Tiempo en espera
+    let [averageTime, setAverageTime] = useState(0); // Tiempo promedio en ejecucion
+    let [totalTime, setTotalTime] = useState(0); // Tiempo total en el sistema
+
+    // funcion para crear una tarea
     const createTask = () => {
         let temporalStatus: string = "en espera";
         let nameArray: string = "tasks";
@@ -49,6 +55,9 @@ function App() {
             case "tasksCanceled":
                 setTasksCanceled([...tasksCanceled, task]);
                 break;
+            case "tasksDone":
+                setTasksDone([...tasksDone, task]);
+                break;
             default:
                 break;
         }
@@ -63,7 +72,8 @@ function App() {
         if (temporalTasks.length > 0) {
             const timer = setTimeout(() => {
                 temporalTasks[0].status = "Completada";
-                setTasksDone([...tasksDone, temporalTasks[0]]);
+                addTask(temporalTasks[0], "tasksDone");
+                //setTasksDone([...tasksDone, temporalTasks[0]]);
                 temporalTasks.shift();
 
                 if (temporalTasks.length > 0) {
@@ -71,16 +81,72 @@ function App() {
                 }
 
                 setTasks(temporalTasks);
-            }, 3000);
+            }, tasks[0].time * tiempo);
 
             return () => clearTimeout(timer);
         }
     }, [tasks]);
 
+    useEffect(() => {
+        let temporalStandby = standby;
+        let temporalAverageTime = averageTime;
+        let temporalTotalTime = totalTime;
+        //debugger;
+        let valueSum = 0;
+        let valueSum2 = 0;
+
+        // Calcular tiempo promedio en ejecucion
+        if (tasksDone.length > 0) {
+            temporalAverageTime += tasksDone[tasksDone.length - 1].time;
+        }
+
+        // Calcular tiempo promedio en espera
+        if (tasksDone.length == 0) {
+            temporalStandby = 0;
+        } else if (tasksDone.length > 1) {
+            valueSum = temporalStandby + tasksDone[tasksDone.length - 2].time;
+            temporalStandby += valueSum;
+        }
+
+        if (tasksDone.length > 0) {
+            valueSum2 = tasksDone[tasksDone.length - 1].time + valueSum;
+            temporalTotalTime += valueSum2;
+        }
+
+        setAverageTime(temporalAverageTime);
+        setStandby(temporalStandby);
+        setTotalTime(temporalTotalTime);
+    }, [tasksDone]);
+
     return (
         <main className="mainSection">
+            <h1 className="TitlePage">Ejemplo visual Algoritmo FIFO</h1>
+
+            <fieldset className="configuration">
+                <legend>Configuracion tiempo de ejecucion</legend>
+                <label>
+                    <input
+                        type="radio"
+                        name="tiempo"
+                        onClick={() => {
+                            setTiempo(1000);
+                        }}
+                        defaultChecked={tiempo == 1000}></input>
+                    Segundos
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        name="tiempo"
+                        onClick={() => {
+                            setTiempo(100);
+                        }}
+                        defaultChecked={tiempo == 100}></input>
+                    Segundos * 10^-1
+                </label>
+            </fieldset>
+
             <button className="button" onClick={() => createTask()}>
-                {" "}
                 Crear tarea
             </button>
 
@@ -89,7 +155,7 @@ function App() {
                     <h2 className="titleSection">Backlog</h2>
                     <ul className="listTask">
                         <li className="titleList">
-                            <p>Nombre Tarea</p>
+                            <p>Tarea</p>
                             <p>tamaño</p>
                             <p>Tiempo</p>
                             <p>Estado</p>
@@ -109,7 +175,7 @@ function App() {
                     <h2 className="titleSection">Completadas</h2>
                     <ul className="listTask">
                         <li className="titleList">
-                            <p>Nombre Tarea</p>
+                            <p>Tarea</p>
                             <p>tamaño</p>
                             <p>Tiempo</p>
                             <p>Estado</p>
@@ -122,6 +188,33 @@ function App() {
                                 <p>{task.status}</p>
                             </li>
                         ))}
+                        <li className="resultTime--first">
+                            <p>Tiempo promedio en espera:</p>
+                            <p>
+                                {standby /
+                                    (tasksDone.length > 0
+                                        ? tasksDone.length
+                                        : 1)}
+                            </p>
+                        </li>
+                        <li className="resultTime">
+                            <p>Tiempo promedio en ejecucion:</p>
+                            <p>
+                                {averageTime /
+                                    (tasksDone.length > 0
+                                        ? tasksDone.length
+                                        : 1)}
+                            </p>
+                        </li>
+                        <li className="resultTime">
+                            <p>Tiempo promedio en el sistema:</p>
+                            <p>
+                                {totalTime /
+                                    (tasksDone.length > 0
+                                        ? tasksDone.length
+                                        : 1)}
+                            </p>
+                        </li>
                     </ul>
                 </section>
 
@@ -129,7 +222,7 @@ function App() {
                     <h2 className="titleSection">Canceladas</h2>
                     <ul className="listTask">
                         <li className="titleList">
-                            <p>Nombre Tarea</p>
+                            <p>Tarea</p>
                             <p>tamaño</p>
                             <p>Tiempo</p>
                             <p>Estado</p>
